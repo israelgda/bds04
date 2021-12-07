830d0226a840e7e9a5e1c0aa27929e36ada9a8a3
Israel Gadelha <gdelha.israel@gmail.com>
2021-12-06 21:12:32 -0300

    Feat: implementadas configuração de Auth e ResourceServer

--- src/main/java/com/devsuperior/bds04/entities/User.java
+++ src/main/java/com/devsuperior/bds04/entities/User.java
@@ -8,6 +8,9 @@ import java.util.stream.Collectors;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -22,6 +25,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 public class User implements UserDetails{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(unique = true)
